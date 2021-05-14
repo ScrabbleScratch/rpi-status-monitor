57c609282b029de237e4264df02179f7386a02ac
ScrabbleScratch <55005341+ScrabbleScratch@users.noreply.github.com>
2021-05-14 15:22:14 -0600

    Update and rename Status.py to status-monitor.py
    Added support for a 4 channel relay module, including a controlled fan connected to channel 4 in module

--- status-monitor.py
+++ status-monitor.py
@@ -1,4 +1,4 @@
-import I2C_LCD_driver as driver     #modified script to work on Ubuntu
+import I2C_LCD_driver_UBUNTU as driver     #modified script to work on Ubuntu
 import RPi.GPIO as gpio             #to take control of the gpio pins
 import socket                       #to get ip address
 import psutil                       #to read system stats
@@ -7,19 +7,44 @@ from time import sleep              #to ake pauses between reads
 lcd = driver.lcd()
 
 gpio.setmode(gpio.BCM)              #set gpio label to BCM
-gpio.setup(17, gpio.OUT)            #set gpio pin 17 as output for fan
 
-on_temp = 50                        #temperature to turn fan on
-off_temp = 45                       #temperature to turn fan off
+#define names for gpio pins
+cooler_pin = 17
+relay1_pin = 1
+relay2_pin = 7
+relay3_pin = 8
+fan_pin = 25    #relay4 pin
 
-cpu = None                          #variable to save last cpu value
-ram = None                          #variable to save last ram value
-temp = None                         #variable to save last temperature
-fan = None                          #variable to save last fan state
-ip = None                           #variable to save last ip address
+#set gpio pins as outputs
+gpio.setup(cooler_pin, gpio.OUT)
+gpio.setup(relay1_pin, gpio.OUT)
+gpio.setup(relay2_pin, gpio.OUT)
+gpio.setup(relay3_pin, gpio.OUT)
+gpio.setup(fan_pin, gpio.OUT)
 
-def refresh(tcpu=0, tram=0, ttemp=0, tfan="", tip=""):
-    global cpu, ram, temp, fan, ip
+#define temperatures to turn cooler and fan on/off
+cooler_on = 50
+cooler_off = 40
+fan_on =55
+fan_off =50
+
+#variables to save last states read
+cpu = None
+ram = None
+temp = None
+cooler = None
+fan = None
+ip = None
+
+#seconds between cycles
+cycle = 5
+
+#get the hostname
+hostname = socket.gethostname()
+
+#lcd control
+def refresh(tcpu=0, tram=0, ttemp=0, tcooler="", tfan="", tip=""):
+    global cpu, ram, temp, cooler, fan, ip
     if cpu != tcpu:                                 #update cpu if changed
         cpu = tcpu
         lcd.lcd_print(" "*10, 1, 0)
@@ -32,15 +57,19 @@ def refresh(tcpu=0, tram=0, ttemp=0, tfan="", tip=""):
         temp = ttemp
         lcd.lcd_print(" "*10, 2, 0)
         lcd.lcd_print(f"TMP:{temp}C", 2, 0)
+    if cooler != tcooler:                           #update cooler if changed
+        cooler = tcooler
+        lcd.lcd_print(" "*10, 3, 0)
+        lcd.lcd_print(f"CLR:{tcooler}", 3, 0)
     if fan != tfan:                                 #update fan if changed
         fan = tfan
-        lcd.lcd_print(" "*10, 2, 10)
-        lcd.lcd_print(f"FAN:{fan}", 2, 10)
+        lcd.lcd_print(" "*10, 3, 10)
+        lcd.lcd_print(f"FAN:{tfan}", 3, 10)
     if ip != tip:                                   #update ip address if changed
         ip = tip
         lcd.lcd_print(" "*20, 4, 0)
         lcd.lcd_print(f"IP:{ip}", 4, 0)
-    print(f"CPU:{cpu}%\tRAM:{ram}G\tTMP:{temp}C\tFAN:{fan}\tIP:{ip}")
+    print(f"CPU:{cpu}%\tRAM:{ram}G\tTMP:{temp}C\tCLR:{cooler}\tFAN:{fan}\tIP:{ip}")
     return
 
 motd = """\
@@ -60,16 +89,26 @@ while True:
     tcpu = round(psutil.cpu_percent())                                      #get cpu percent
     tram = round(psutil.virtual_memory()[3]*(10**-9), 1)                    #get ram percent converted from byte to gigabyte
     ttemp = round(psutil.sensors_temperatures()["cpu_thermal"][0][1], 1)    #get cpu temperature
-    tip = socket.gethostbyname(socket.gethostname())                        #get ip address
+    tip = socket.gethostbyname(hostname)                                    #get ip address
+    if cooler:
+        if ttemp >= cooler_on:
+            gpio.output(cooler_pin, True)
+            tcooler = "ON"
+        elif ttemp <= cooler_off:
+            gpio.output(cooler_pin, False)
+            tcooler = "OFF"
+    else:
+        gpio.output(cooler_pin, False)
+        tcooler = "OFF"
     if fan:
-        if ttemp >= on_temp:
-            gpio.output(17, True)
+        if ttemp >= fan_on:
+            gpio.output(fan_pin, True)
             tfan = "ON"
-        elif ttemp <= off_temp:
-            gpio.output(17, False)
+        elif ttemp <= fan_off:
+            gpio.output(fan_pin, False)
             tfan = "OFF"
     else:
-        gpio.output(17, False)
+        gpio.output(fan_pin, False)
         tfan = "OFF"
-    refresh(tcpu, tram, ttemp, tfan, tip)
-    sleep(5)
+    refresh(tcpu, tram, ttemp, tcooler, tfan, tip)
+    sleep(cycle)
